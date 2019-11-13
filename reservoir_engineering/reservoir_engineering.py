91e9aa1337759ce59002606980a2f2d124524e85
Guillermo Garcia <guilleconu@gmail.com>
2019-11-13 16:57:33 +0200

    Updated RE and AD

---
+++ reservoir_engineering/reservoir_engineering.py
@@ -0,0 +1,152 @@
+"""
+Circuit preparation for the markovian reservoir engineering
+"""
+
+from qiskit import QuantumCircuit
+import numpy as np
+
+def initial_conditions(q, system):
+    """Returns a QuantumCircuit implementing ZZ pump channel on the system qubits
+    
+    Args:
+        q (QuantumRegister): the register to use for the circuit
+        system (list): list of indices for the system qubits
+    
+    Returns:
+        A dictionary with the initial state QuantumCircuit objects and a list of labels
+    """
+    # State labels
+    state_labels = ['00', '01', '10', '11']
+    
+    ic = {}
+    for ic_label in state_labels:
+        ic[ic_label] = QuantumCircuit(q)
+    
+    # |01>
+    ic['01'].x(q[system[0]])
+    
+    # |10>
+    ic['10'].x(q[system[1]])
+    
+    # |11>
+    ic['11'].x(q[system[0]])
+    ic['11'].x(q[system[1]])
+    
+    return ic
+
+def zz_pump(q, c, p, system, ancilla):
+    """Returns a QuantumCircuit implementing ZZ pump channel on the system qubits
+    
+    Args:
+        q (QuantumRegister): the register to use for the circuit
+        c (ClassicalRegister): the register to use for the measurement of the system qubits
+        p (float): the efficiency for the channel between 0 and 1
+        system (list): list of indices for the system qubits
+        ancilla (int): index for the ancillary qubit
+    
+    Returns:
+        A QuantumCircuit object
+    """
+    zz = QuantumCircuit(q, c)
+    
+    theta = 2 * np.arcsin(np.sqrt(p))
+    
+    # Map information to ancilla
+    zz.cx(q[system[0]], q[system[1]])
+    zz.x(q[ancilla])
+    zz.cx(q[system][1], q[ancilla])
+    
+    # Conditional rotation
+    zz.cu3(theta, 0.0, 0.0, q[ancilla], q[system[1]])
+    
+    # Inverse mapping
+    zz.cx(q[system[1]], q[ancilla])
+    
+    # Measurement
+    zz.h(q[system[0]])
+    zz.measure(q[system[0]], c[0])
+    zz.measure(q[system[1]], c[1])
+    
+    return zz
+
+def xx_pump(q, c, p, system, ancilla):
+    """Returns a QuantumCircuit implementing XX pump channel on the system qubits
+    
+    Args:
+        q (QuantumRegister): the register to use for the circuit
+        c (ClassicalRegister): the register to use for the measurement of the system qubits
+        p (float): the efficiency for the channel between 0 and 1
+        system (list): list of indices for the system qubits
+        ancilla (int): index for the ancillary qubit
+    
+    Returns:
+        A QuantumCircuit object
+    """
+    xx = QuantumCircuit(q, c)
+
+    theta = 2 * np.arcsin(np.sqrt(p))
+    
+    # Map information to ancilla
+    xx.cx(q[system[0]], q[system[1]])
+    xx.h(q[system[0]])
+    xx.x(q[ancilla])
+    xx.cx(q[system[0]], q[ancilla])
+    
+    # Conditional rotation
+    xx.cu3(theta, 0.0, 0.0, q[ancilla], q[system[0]])
+    
+    # Inverse mapping
+    xx.cx(q[system[0]], q[ancilla])
+    
+    # Measurement
+    xx.measure(q[system[0]], c[0])
+    xx.measure(q[system[1]], c[1])
+    
+    return xx
+
+def zz_xx_pump(q, c, p, system, ancillae):
+    """Returns a QuantumCircuit implementing ZZ pump channel on the system qubits
+    
+    Args:
+        q (QuantumRegister): the register to use for the circuit
+        c (ClassicalRegister): the register to use for the measurement of the system qubits
+        p (float): the efficiency for the channel between 0 and 1
+        system (list): list of indices for the system qubits
+        ancillae (list): list of indices for the ancillary qubits
+    
+    Returns:
+        A QuantumCircuit object
+    """
+    zx = QuantumCircuit(q, c)
+    
+    theta = 2 * np.arcsin(np.sqrt(p))
+    
+    ## ZZ pump
+    # Map information to ancilla
+    zx.cx(q[system[0]], q[system[1]])
+    zx.x(q[ancillae[0]])
+    zx.cx(q[system[1]], q[ancillae[0]])
+    
+    # Conditional rotation
+    zx.cu3(theta, 0.0, 0.0, q[ancillae[0]], q[system[1]])
+    
+    # Inverse mapping
+    zx.cx(q[system[1]], q[ancillae[0]])
+    
+    ## XX pump
+    # Map information to ancilla
+    zx.h(q[system[0]])
+    zx.x(q[ancillae[1]])
+    zx.cx(q[system[0]], q[ancillae[1]])
+    
+    # Conditional rotation
+    zx.cu3(theta, 0.0, 0.0, q[ancillae[1]], q[system[0]])
+    
+    # Inverse mapper
+    zx.cx(q[system[0]], q[ancillae[1]])
+    
+    # Measurement
+    zx.measure(q[system[0]], c[0])
+    zx.measure(q[system[1]], c[1])
+    
+    return zx
